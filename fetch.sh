5eb2f7570a60eb120d5b0cea381eadb3c581d63a
Avish K <49350400+AK1089@users.noreply.github.com>
2024-03-06 23:47:09 +0000

    Create fetch.sh

---
+++ fetch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo -e "Now running shell script \033[0;31;1mfetch.sh\033[0m."
+
+cd /home/pi/cassette-project
+rm -f script.py
+
+echo -e -n "Downloading latest version of \033[0;31;1mscript.py...\033[0m"
+
+if wget -q --no-cache https://raw.githubusercontent.com/AK1089/digital-cassettes/main/script.py; then
+    echo " succeeded!"
+else
+    echo " failed."
+    exit 1
+fi
+
+echo -e "Now running Python script \033[0;31;1mfetch.py\033[0m."
+python3 fetch.py
+
+echo -e "Now running Python script \033[0;31;1mscript.py\033[0m."
+python3 script.py
