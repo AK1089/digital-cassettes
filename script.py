82498db4f97525902ad88a588667d5f3243fab43
Avish K <49350400+AK1089@users.noreply.github.com>
2024-03-07 17:07:43 +0000

    adds remote update without rebooting

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 import disnake
 from disnake.ext import commands
 from os import getenv, system
+from sys import exit
 
 # lets us know in the console (for manual runs) that we're in
 print("[Python] Bot started!")
@@ -24,6 +25,19 @@ async def reboot(ctx: commands.Context):
         system('sudo reboot')
     else:
         await ctx.send("You do not have permission to perform this action.")
+
+
+# updates the Pi with the new code
+@bot.command(name="update")
+async def update(ctx: commands.Context):
+
+    # once again, only I'm allowed to do this!
+    if await bot.is_owner(ctx.author):
+        await ctx.send("Restarting bot with updated script from GitHub...")
+        exit(1)  # nonzero code so fetch.service restarts fetch.sh
+    else:
+        await ctx.send("You do not have permission to perform this action.")
+
 
 # ignores errors caused by nonexistent commands
 @bot.event
