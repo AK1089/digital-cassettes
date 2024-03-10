2e376e1ba8d28309e174c458d586bb7798a05738
Avish K <49350400+AK1089@users.noreply.github.com>
2024-03-10 18:45:56 +0000

    Update fetch.py
    fetch.py is now the discord bot

--- fetch.py
+++ fetch.py
@@ -1,7 +1,63 @@
+# a very very basic bot
+import disnake
+from disnake.ext import commands
+from os import getenv, system
+from sys import exit
 from datetime import datetime
 
+# prepends a line to the restart log with the restart time
 with open("restart_log.txt", "r+") as f:
     text = f.read()
     line_count = text.count("\n") + 1
     f.seek(0)
     f.write(f"Reboot: {datetime.now():%Y-%m-%d %H:%M:%S} | #{line_count}\n" + text)
+
+# lets us know in the console (for manual runs) that we're in
+print("[Python] Bot started!")
+
+# prefix > for commands, and all intents
+bot: commands.Bot = commands.Bot(">", intents=disnake.Intents.all(),
+                                 activity=disnake.Activity(type=disnake.ActivityType.playing, name=f"Rebooted {datetime.now():%H:%M:%S}"))
+
+
+# repeats what you say
+@bot.command(name="echo")
+async def echo(ctx: commands.Context, *text):
+    await ctx.send(" ".join(text))
+
+# reboots the Pi on call (so we can do it remotely)
+@bot.command(name="reboot")
+async def reboot(ctx: commands.Context):
+
+    # only I'm allowed to do this!
+    if await bot.is_owner(ctx.author):
+        await ctx.send("Rebooting now...")
+        system('sudo reboot')
+    else:
+        await ctx.send("You do not have permission to perform this action.")
+
+
+# updates the Pi with the new code
+@bot.command(name="update")
+async def update(ctx: commands.Context):
+
+    # once again, only I'm allowed to do this!
+    if await bot.is_owner(ctx.author):
+        await ctx.send("Restarting bot with updated script from GitHub...")
+        exit(1)  # nonzero code so fetch.service restarts fetch.sh
+    else:
+        await ctx.send("You do not have permission to perform this action.")
+
+
+# ignores errors caused by nonexistent commands
+@bot.event
+async def on_command_error(ctx: commands.Context, error):
+    if isinstance(error, commands.errors.CommandNotFound): return
+    else: raise error
+
+# removes the unnecessary help command
+@bot.event
+async def on_ready():
+    bot.remove_command("help")
+
+bot.run(getenv("DISCORD_BOT_TOKEN"))
