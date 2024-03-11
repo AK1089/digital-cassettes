495fb81e4c745afe140a1fe27c7576a4b32eb7a8
Avish K <49350400+AK1089@users.noreply.github.com>
2024-03-11 14:41:02 +0000

    everything runs at the right time now

--- script.py
+++ script.py
@@ -1,8 +1,2 @@
-# writes a new file every 10 seconds
-from time import sleep
-
-i = 0
-while (i := i + 1):
-    with open(f"dummy_file_{i}.txt", "w+") as f:
-        pass
-    sleep(10)
+# does nothing now that I know this is successfully running
+pass
